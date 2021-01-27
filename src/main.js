0e2fa08660f6508de820deace6f0bce31dc5f5e7
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-01-27 21:10:38 +0200

    adding sort function

--- src/main.js
+++ src/main.js
@@ -6,14 +6,15 @@ function contentLoaded(){
     const prioritySelector = document.getElementById("priority-selector");
     const counter = document.getElementById("counter");
     const sortButton = document.getElementById("sort-button");
+    const list = document.getElementById("mission-list");
     addButton.addEventListener("click", addToViewSection);
     sortButton.addEventListener("click", sortTheMissions);
     counter.textContent = count + " TODOS";
+    let data = [];
 
     function addToViewSection(){
         //setting all the variables
         const mission = document.getElementById("text-input");
-        const list = document.getElementById("mission-list");
         const container = document.createElement("div");
         const listItem = document.createElement("li");
         const toDoText = document.createElement("div");
@@ -28,6 +29,12 @@ function contentLoaded(){
         priority.setAttribute("class", "todo-priority")
         toDoText.setAttribute("class", "todo-text")
         toDoCreatedAt.setAttribute("class", "todo-created-at")
+        const missionData = {
+            text: mission.value,
+            time: getSQLFormat(),
+            priority: prioritySelector.value
+        };
+        data.unshift(missionData);
 
         //appending the new variables to their spot
         container.append(priority);
@@ -45,12 +52,44 @@ function contentLoaded(){
         mission.focus();
     }
 
-
-    function getSQLFormat(){
-        return new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() ;
-    }
-
     function sortTheMissions(){
-        
+        list.textContent = "";
+        data.sort(function(a, b){
+            if(a.priority > b.priority){
+                return -1;
+            }
+            if(a.priority < b.priority){
+                return 1;
+            }
+            return 0;
+        });
+        for (const orderedData of data) {
+            createNewList(orderedData);
+        }
     }
+    
+    function createNewList(itemData){
+        const container = document.createElement("div");
+        const listItem = document.createElement("li");
+        const toDoText = document.createElement("div");
+        const toDoCreatedAt = document.createElement("div");
+        const priority = document.createElement("div");
+    
+        toDoText.innerText = itemData.text;
+        toDoCreatedAt.innerText = itemData.time;
+        priority.innerText = itemData.priority;
+        container.setAttribute("class", "todo-container");
+        priority.setAttribute("class", "todo-priority");
+        toDoText.setAttribute("class", "todo-text");
+        toDoCreatedAt.setAttribute("class", "todo-created-at");
+        container.append(priority);
+        container.append(toDoCreatedAt);
+        container.append(toDoText);
+        listItem.append(container);
+        list.append(listItem);
+    }
+}
+
+function getSQLFormat(){
+    return new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() ;
 }
