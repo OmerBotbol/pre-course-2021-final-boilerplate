0494fab5d80f099bbd36f5871b5cdbd123f2a003
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-02-17 09:23:04 +0200

    adding second get
    filter isn't working

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const {readFileSync} = require('fs');
 const data = readFileSync("./tasks.json", {encoding: 'utf8', flag: 'r'})
+console.log (data);
 
 app.get('/b', (req, res) =>{
     res.send(data)
 });
+app.get('/b/:id', (req, res) => {
+    const filteredTask = data.filter(task => task.id === req.params.id);
+    res.send(filteredTask);
+})
 
 app.listen(3000);
