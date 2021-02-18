9618f859633fa4315b264e5713a350d123edf8d5
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-02-18 08:45:14 +0200

    adding try/catch

--- backend/index.js
+++ backend/index.js
@@ -7,37 +7,56 @@ const {readFileSync} = require('fs');
 app.use(express.json());
 
 app.get('/b', (req, res) =>{
-    const listOfTasks=[];
-    fs.readdirSync('./task').forEach(file => {
-        let task = JSON.parse(readFileSync(`./task/${file}`, {encoding: 'utf8', flag: 'r'}))
-        listOfTasks.push(task)
-    });
-    res.send(listOfTasks);
+    try{
+        const listOfTasks=[];
+        fs.readdirSync('./task').forEach(file => {
+            let task = JSON.parse(readFileSync(`./task/${file}`, {encoding: 'utf8', flag: 'r'}))
+            listOfTasks.push(task)
+        });
+        res.send(listOfTasks);
+    } catch(e){
+        res.status(404).json({massage: e})
+    }
 });
 app.get('/b/:id', (req, res) => {
-    fs.readdirSync('./task').forEach(file => {
-        if(file === req.params.id){
-            res.send(JSON.parse(readFileSync(`./task/${file}`, {encoding: 'utf8', flag: 'r'})))
-        }
-    });
+    try{
+        fs.readdirSync('./task').forEach(file => {
+            if(file === req.params.id){
+                res.send(JSON.parse(readFileSync(`./task/${file}`, {encoding: 'utf8', flag: 'r'})))
+            }
+        });
+    } catch(e){
+        res.status(404).json({massage: e})
+    }
 })
 
 app.post('/b', (req, res) =>{
-    const body = JSON.stringify(req.body, null, 4);
-    fs.writeFileSync(`./task/${Date.now()}.json`, body);
-    res.send(body);
-
+    try{
+        const body = JSON.stringify(req.body, null, 4);
+        fs.writeFileSync(`./task/${Date.now()}.json`, body);
+        res.send(body);
+    } catch{
+        res.status(500).json({massage: e})
+    }
 });
 
 app.put('/b/:id', (req, res) => {
-    const body = JSON.stringify(req.body, null, 4);
-    fs.writeFileSync(`./task/${req.params.id}`, body);
-    res.send('update success');
+    try{
+        const body = JSON.stringify(req.body, null, 4);
+        fs.writeFileSync(`./task/${req.params.id}`, body);
+        res.send('update success');
+    } catch(e){
+        res.status(500).json({massage: e})
+    }
 })
 
 app.delete('/b/:id', (req, res) => {
-    fs.unlinkSync(`./task/${req.params.id}`);
-    res.send('the file has deleted');
+    try{
+        fs.unlinkSync(`./task/${req.params.id}`);
+        res.send('the file has deleted');
+    } catch{
+        res.status(404).json({massage: e})
+    }
 })
 
 app.listen(3000, console.log("listening to port 3000"));
